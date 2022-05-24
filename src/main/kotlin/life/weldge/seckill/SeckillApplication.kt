2b2911db81c286333f3e567fe827b7727fc57e5a
weldge <chenweijie@ctirobot.com>
2022-05-24 19:10:19 +0800

    Initial commit

---
+++ src/main/kotlin/life/weldge/seckill/SeckillApplication.kt
@@ -0,0 +1,11 @@
+package life.weldge.seckill
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SeckillApplication
+
+fun main(args: Array<String>) {
+	runApplication<SeckillApplication>(*args)
+}
---
+++ src/test/kotlin/life/weldge/seckill/SeckillApplicationTests.kt
@@ -0,0 +1,13 @@
+package life.weldge.seckill
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SeckillApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
