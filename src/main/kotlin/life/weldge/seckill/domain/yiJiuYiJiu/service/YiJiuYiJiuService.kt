9d0150d98a0b84622680d79f990eaff48129f1fd
weijie.chen <weijie.chen@ctirobot.com>
2022-06-27 17:21:16 +0800

    update 1919 seckill logic

--- src/main/kotlin/life/weldge/seckill/domain/yiJiuYiJiu/service/YiJiuYiJiuService.kt
+++ src/main/kotlin/life/weldge/seckill/domain/yiJiuYiJiu/service/YiJiuYiJiuService.kt
@@ -3,14 +3,11 @@ package life.weldge.seckill.domain.yiJiuYiJiu.service
 import com.google.common.collect.ImmutableMap
 import io.appium.java_client.AppiumBy
 import life.weldge.seckill.config.DriverYiJiuYiJiu
-import life.weldge.seckill.domain.suning.service.SuningService
 import life.weldge.seckill.domain.yiJiuYiJiu.vo.YiJiuYiJiuReserveResult
 import life.weldge.seckill.domain.yiJiuYiJiu.vo.YiJiuYiJiuSeckillResult
-import org.openqa.selenium.support.ui.ExpectedConditions
-import org.openqa.selenium.support.ui.WebDriverWait
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
-import java.time.Duration
+import java.time.LocalDateTime
 import java.util.concurrent.TimeUnit
 
 @Service
@@ -29,36 +26,39 @@ class YiJiuYiJiuService(
                 //点击我的预约，进入预约列表
                 it.executeScript("mobile: clickGesture", ImmutableMap.of("x", 580, "y", 2115))
                 TimeUnit.SECONDS.sleep(3L)
-                it.findElement(AppiumBy.className("android.widget.Button"))?.let {detailElement ->
-                    if (detailElement.text == "待抢购") {
-                        //点击进入茅台详情页
-                        detailElement.click()
-                        TimeUnit.SECONDS.sleep(1L)
-                        //点击数量-进入弹窗
-                        it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/layout_product_number")).click()
-                        TimeUnit.SECONDS.sleep(1L)
-                        //点击减少符号-修改抢购数量为1
-                        it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/btnDecrease")).click()
-                        TimeUnit.SECONDS.sleep(1L)
-                        //关闭数量弹窗-回到详情页，等待抢购
-                        it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/iconClose")).click()
-
-                        //点击立即抢购-进入弹窗页
-                        it.findElement(
-                            AppiumBy.id("com.yijiuyijiu.eshop:id/tv_buy")
-                        ).click()
-                        //点击弹窗页面-立即抢购
-                        it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/btnBuy")).click()
-                        //判断抢购结果弹窗
-                        it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/titleTv")).let { resultElement ->
-                            if (resultElement.text == "抢购失败") {
-                                //关闭
-                                it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/confirmTextView")).click()
+                //点击待抢购-进入茅台详情页
+                it.executeScript("mobile: clickGesture", ImmutableMap.of("x", 900, "y", 623))
+                TimeUnit.SECONDS.sleep(2L)
+                //点击数量-进入弹窗
+                it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/layout_product_number")).click()
+                TimeUnit.SECONDS.sleep(1L)
+                //点击减少符号-修改抢购数量为1
+                it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/btnDecrease")).click()
+                TimeUnit.SECONDS.sleep(1L)
+                //关闭数量弹窗-回到详情页，等待抢购
+                it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/iconClose")).click()
+                while (true) {
+                    //点击立即抢购-进入弹窗页
+                    it.findElement(
+                        AppiumBy.id("com.yijiuyijiu.eshop:id/tv_buy")
+                    )?.let { element ->
+                        //判断抢购是否已开始
+                        if(element.text == "立即抢购") {
+                            element.click()
+                            //点击弹窗页面-立即抢购
+                            it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/btnBuy")).click()
+                            //判断抢购结果弹窗
+                            it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/titleTv")).let { resultElement ->
+                                if (resultElement.text == "抢购失败") {
+                                    //关闭
+                                    it.findElement(AppiumBy.id("com.yijiuyijiu.eshop:id/confirmTextView")).click()
+                                }else {
+                                    return YiJiuYiJiuSeckillResult.success()
+                                }
                             }
-                            return YiJiuYiJiuSeckillResult.success()
                         }
+                        if (LocalDateTime.now().minute >= 20) return YiJiuYiJiuSeckillResult.fails()
                     }
-
                 }
                 return YiJiuYiJiuSeckillResult.fails()
             } catch (e: Exception) {
--- src/test/kotlin/life/weldge/seckill/appium/YiJiuYiJiuTests.kt
+++ src/test/kotlin/life/weldge/seckill/appium/YiJiuYiJiuTests.kt
@@ -1,38 +1,38 @@
-//package life.weldge.seckill.appium
-//
-//import org.junit.jupiter.api.Test
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.context.SpringBootTest
-//import life.weldge.seckill.config.DriverYiJiuYiJiu
-//import life.weldge.seckill.domain.ResultState
-//import life.weldge.seckill.domain.yiJiuYiJiu.service.YiJiuYiJiuService
-//import java.util.concurrent.TimeUnit
-//
-//@SpringBootTest
-//class YiJiuYiJiuTests {
-//
-//    @Autowired
-//    private val driverJd: DriverYiJiuYiJiu? = null
-//
-//    @Autowired
-//    private val service: YiJiuYiJiuService? = null
-//
-//    @Test
-//    fun connect() {
-//        driverJd!!.getAndroidDriver().let {
-//            TimeUnit.SECONDS.sleep(5L)
-//            it.closeApp()
-//        }
-//    }
-//
-//    @Test
-//    fun seckillMaotai() {
-//        assert(ResultState.SUCCESS == service!!.seckillMaotai().result)
-//    }
-//
-//    @Test
-//    fun reserveMaotai() {
-//        assert(ResultState.SUCCESS == service!!.reserveMaotai().result)
-//    }
-//
-//}
+package life.weldge.seckill.appium
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import life.weldge.seckill.config.DriverYiJiuYiJiu
+import life.weldge.seckill.domain.ResultState
+import life.weldge.seckill.domain.yiJiuYiJiu.service.YiJiuYiJiuService
+import java.util.concurrent.TimeUnit
+
+@SpringBootTest
+class YiJiuYiJiuTests {
+
+    @Autowired
+    private val driverJd: DriverYiJiuYiJiu? = null
+
+    @Autowired
+    private val service: YiJiuYiJiuService? = null
+
+    @Test
+    fun connect() {
+        driverJd!!.getAndroidDriver().let {
+            TimeUnit.SECONDS.sleep(5L)
+            it.closeApp()
+        }
+    }
+
+    @Test
+    fun seckillMaotai() {
+        assert(ResultState.SUCCESS == service!!.seckillMaotai().result)
+    }
+
+    @Test
+    fun reserveMaotai() {
+        assert(ResultState.SUCCESS == service!!.reserveMaotai().result)
+    }
+
+}
