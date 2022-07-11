5c2ebd1b9ab611fc5c003795ea4ae814b1844438
weijie.chen <weijie.chen@ctirobot.com>
2022-07-11 11:02:37 +0800

    icon update

--- src/test/kotlin/life/weldge/seckill/appium/ReserveTriggerTests.kt
+++ src/test/kotlin/life/weldge/seckill/appium/ReserveTriggerTests.kt
@@ -1,24 +1,24 @@
-//package life.weldge.seckill.appium
-//
-//import org.junit.jupiter.api.Test
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.boot.test.context.SpringBootTest
-//import life.weldge.seckill.scheduled.ReserveTrigger
-//
-//@SpringBootTest
-//class JdTests {
-//
-//    @Autowired
-//    private val reserveTrigger: ReserveTrigger? = null
-//
-//    @Test
-//    fun one() {
-//        reserveTrigger!!.reserveOne()
-//    }
-//
-//    @Test
-//    fun two() {
-//        reserveTrigger!!.reserveTwo()
-//    }
-//
-//}
+package life.weldge.seckill.appium
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import life.weldge.seckill.scheduled.ReserveTrigger
+
+@SpringBootTest
+class ReserveTriggerTests {
+
+    @Autowired
+    private val reserveTrigger: ReserveTrigger? = null
+
+    @Test
+    fun one() {
+        reserveTrigger!!.reserveOne()
+    }
+
+    @Test
+    fun two() {
+        reserveTrigger!!.reserveTwo()
+    }
+
+}
