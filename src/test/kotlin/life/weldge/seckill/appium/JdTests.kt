6b317ffe3ca8f553ac6d34c0ab30454a9bf5be85
weijie.chen <weijie.chen@ctirobot.com>
2022-07-11 11:03:34 +0800

    update tests

--- src/test/kotlin/life/weldge/seckill/appium/JdTests.kt
+++ src/test/kotlin/life/weldge/seckill/appium/JdTests.kt
@@ -1,35 +1,35 @@
-package life.weldge.seckill.appium
-
-import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
-import life.weldge.seckill.config.DriverJd
-import life.weldge.seckill.domain.ResultState
-import life.weldge.seckill.domain.jd.service.JdService
-
-@SpringBootTest
-class JdTests {
-
-    @Autowired
-    private val driverJd: DriverJd? = null
-
-    @Autowired
-    private val service: JdService? = null
-
-    @Test
-    fun connect() {
-        var driver = driverJd!!.getAndroidDriver()
-    }
-
-    @Test
-    fun seckillMaotai() {
-
-        assert(ResultState.SUCCESS == service!!.seckillMaotai().result)
-    }
-
-    @Test
-    fun reserveMaotai() {
-        assert(ResultState.SUCCESS == service!!.reserveMaotai().result)
-    }
-
-}
+//package life.weldge.seckill.appium
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.beans.factory.annotation.Autowired
+//import org.springframework.boot.test.context.SpringBootTest
+//import life.weldge.seckill.config.DriverJd
+//import life.weldge.seckill.domain.ResultState
+//import life.weldge.seckill.domain.jd.service.JdService
+//
+//@SpringBootTest
+//class JdTests {
+//
+//    @Autowired
+//    private val driverJd: DriverJd? = null
+//
+//    @Autowired
+//    private val service: JdService? = null
+//
+//    @Test
+//    fun connect() {
+//        var driver = driverJd!!.getAndroidDriver()
+//    }
+//
+//    @Test
+//    fun seckillMaotai() {
+//
+//        assert(ResultState.SUCCESS == service!!.seckillMaotai().result)
+//    }
+//
+//    @Test
+//    fun reserveMaotai() {
+//        assert(ResultState.SUCCESS == service!!.reserveMaotai().result)
+//    }
+//
+//}
